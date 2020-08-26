c04fa8f5cd0c806754ca1c90eca9874cbeb526c4
Sergey Anufriev <35104456+SergeyAnufriev@users.noreply.github.com>
2020-08-26 14:52:52 +0300

    Add files via upload

---
+++ gan.py
@@ -0,0 +1,87 @@
+import torch
+import torch.nn as nn
+from torch.nn.functional import one_hot
+
+
+def permute3D(A):
+
+  A = A.permute(2,0,1)
+  A_new = torch.triu(A,diagonal=1) + torch.transpose(torch.triu(A,diagonal=1),1,2)
+
+  return A_new.permute(1,2,0)
+
+def permute4D(A):
+
+  bz,n,m,k = A.size()
+
+  A_new = A.permute(1,2,3,0)
+  A_new = A_new.reshape(n,m,-1)
+  A_new = permute3D(A_new)
+  A_new = A_new.reshape(n,m,k,bz)
+  A_new = A_new.permute(3,0,1,2)
+
+  return A_new
+
+def A_mat(data):   ### data to one-hot-encode adjacency tensor
+  edge_type  = data.edge_attr
+  edge_type_ = one_hot(edge_type)
+  n_nodes    = data.num_nodes
+  adj_mat    = torch.zeros((n_nodes,n_nodes,len(torch.unique(edge_type))))
+
+  edge_index = data.edge_index
+  for i in range(data.num_edges):
+    k, m = edge_index[0,i],edge_index[1,i]
+    adj_mat[k,m,:] = edge_type_[i,:]
+
+  return adj_mat.to(dtype = torch.float32)
+
+
+def to_list(bz,n_nodes):
+  final_list = [0]*n_nodes
+  for i in range(1,bz):
+    final_list += [i]*n_nodes
+  return final_list
+
+
+class Generator(nn.Module):
+
+    # N - maximum number of atoms
+    # T - number of atom types
+    # Y - number of bond types
+
+    #### Candidates to enforce A_sym = LL^T
+
+    def __init__(self,z_dim,N,T,Y,temp):
+      super(Generator, self).__init__()
+
+      self.N = N
+      self.T = T
+      self.Y = Y
+
+      self.temp = temp ### GambelSoftmax activation - temperature
+
+      self.lin1 = nn.Linear(z_dim,128)
+      self.lin2 = nn.Linear(128,256)
+      self.lin3 = nn.Linear(256,512)
+
+      self.edges = nn.Linear(512,self.N*self.N*self.Y)
+      self.nodes = nn.Linear(512,self.N*self.T)
+
+      self.act   = nn.functional.gumbel_softmax
+
+    def forward(self,x):
+
+        output      = self.lin3(self.lin2(self.lin1(x)))
+
+        nodes_logit = self.nodes(output).view(-1,self.N,self.T)
+        nodes       = self.act(nodes_logit,dim=-1,tau=self.temp,hard=True)
+
+        edges_logit      = self.edges(output).view(-1,self.N,self.N,self.Y)
+        edges_logit_T    = torch.transpose(edges_logit,1,2)
+        edges_logit      = 0.5*(edges_logit+edges_logit_T)
+
+        edges_logit      = self.act(edges_logit,dim=-1,tau=self.temp,hard=True)
+
+        edges = permute4D(edges_logit)
+
+        return  nodes, edges
---
+++ valid.py
@@ -0,0 +1,111 @@
+from random import choice
+import torch
+
+#### BFS #########################
+
+class Queue:
+    def __init__(self):
+        self.items  = []
+    def enq(self,x):
+        self.items.insert(0,x)
+    def __len__(self):
+        return len(self.items)
+    def deq(self):
+        if len(self.items ) == 0:
+            return None
+        else:
+            return self.items.pop()
+
+def distance(adj_list,start): ### BFS
+
+    q = Queue()
+    colour   = []
+    dist = []
+    parent   = []
+    for i in range(len(adj_list)):
+        if i == start:
+           c = 'GRAY'
+           d = 0
+           p = None
+        else:
+            c = 'WHITE'
+            d = float('inf')
+            p = None
+        colour.append(c)
+        dist.append(d)
+        parent.append(p)
+    q.enq(start)
+    while len(q)!=0:
+        u = q.deq()
+        for v in adj_list[u]:
+            if colour[v] == 'WHITE':
+               colour[v] = 'GRAY'
+               dist[v] = dist[u] + 1
+               parent[v] = u
+               q.enq(v)
+        colour[u] = 'BLACK'
+    return dist
+
+
+def subgraphs(adj_list):
+
+  nodes     = list(range(len(adj_list)))
+  subgraphs = []
+
+  while len(nodes)!=0:
+
+    start = choice(nodes)
+    dist  = distance(adj_list,start)
+    subG  = [i for i,x in enumerate(dist) if x != float('inf')]
+    subgraphs.append(subG)
+    nodes = list(set(nodes)-set(subG))
+
+  return subgraphs
+
+
+def node_to_dict(mat):
+  node_to_val = {}
+  n,m = mat.size()
+  pad = torch.tensor([0]*(m-1) + [1]).type(torch.LongTensor)
+  for i in range(n):
+    if all(mat[i,:] == pad):
+      node_to_val[i] = 0
+    else:
+      node_to_val[i] = 1
+  return node_to_val
+
+
+def tens_to_adj_list(tens):
+  n,_,m = tens.size()
+  adj_l = [[] for _ in range(n)]
+  pad1 = torch.zeros(m).type(torch.float32)
+  pad2 = torch.tensor([0]*(m-1)+[1]).type(torch.float32)
+  for i in range(n):
+    for j in range(n):
+      con_ = tens[i,j,:]
+      data = [torch.equal(con_,pad1),torch.equal(con_,pad2)]
+      if not any(data):
+        adj_l[i].append(j)
+  return adj_l
+
+
+def valid(subgraphs,dict_):
+  n_valid = 0  ### number of graphs with real atoms
+  for G in subgraphs:
+    if len(G)>1 and any(dict_[x] == 0 for x in G):  ## check if empty nodes are self isolated or not
+      return 0
+    elif any(dict_[x] == 1 for x in G):  ### check if real atom present in subgraph
+      n_valid+=1
+  if n_valid == 1:
+    return 1  ### only one subgraph must have real atoms
+  else:
+    return 0
+
+
+def valid_graph(A,x):
+
+  adj_list   = tens_to_adj_list(A)  ## From Adjeccency tensor to adjecency list
+  dict_      = node_to_dict(x)      ## 1 if atom real, 0 empty node
+  subgraphs_ = subgraphs(adj_list)  ## find all subgraphs by BFS algo
+
+  return valid(subgraphs_,dict_)
