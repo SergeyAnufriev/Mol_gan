24af4788124e50c1bc35f69093c00645eb52e61f
zcemg08 <anufriiev.sergii@gmail.com>
2020-09-14 16:43:09 +0300

    fuck this work

--- test1.py
+++ test1.py
@@ -1,5 +1,13 @@
 from rdkit import Chem
+import numpy as np
 
 m = Chem.MolFromSmiles('Cc1ccccc1')
 
 print(dir(m))
+
+
+from scipy.sparse import csr_matrix
+csr_matrix((3, 4), dtype=np.int8).toarray()
+
+
+
