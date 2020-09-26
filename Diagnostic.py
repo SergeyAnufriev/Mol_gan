fa94c34bef2d1c4ee9d5236a3633282f8b443453
Sergey Anufriev <35104456+SergeyAnufriev@users.noreply.github.com>
2020-09-26 16:46:05 +0300

    Update Diagnostic.py

--- Diagnostic.py
+++ Diagnostic.py
@@ -38,3 +38,51 @@ class JacobianVectorProduct(linalg.LinearOperator):
             _hv.append(g.contiguous().view(-1))
         hv = torch.cat(_hv)
         return hv.cpu()
+      
+def vis(G,D):
+  with torch.no_grad():
+
+    fig = go.Figure()
+    G.eval()
+    gen = G(z_fixed)
+    gen = gen.cpu().data.numpy()
+
+    x1_g_min,x1_g_max,x2_g_min,x2_g_max = min_max(gen)
+    x1_d_min,x1_d_max,x2_d_min,x2_d_max = min_max(X_train_save)
+    
+    u,v  = np.meshgrid(np.linspace(min(-5,x1_g_min,x1_d_min),\
+                                   max(5,x1_g_max,x1_d_max),20),\
+                       np.linspace(min(-5,x2_g_min,x2_d_min),\
+                                   max(5,x2_g_max,x2_d_max),20))
+    
+    vals = np.hstack([u.reshape(400,1),v.reshape(400,1)])
+    
+    D.eval()
+    pred = D(torch.tensor(vals).float()).detach().numpy().reshape(20,20)
+
+    fig.add_trace(go.Contour(
+        z=[list(x) for x in list(pred)],
+        x=list(np.linspace(min(-5,x1_g_min,x1_d_min),max(5,x1_g_max,x1_d_max),20)),
+        y=list(np.linspace(min(-5,x2_g_min,x2_d_min),\
+                    max(5,x2_g_max,x2_d_max),20)),\
+        contours=dict(
+            coloring ='heatmap',
+            showlabels = True, 
+            labelfont = dict(size = 12,color = 'white'))
+    ))
+
+
+    fig.add_trace(go.Scatter(x=X_train_save[:, 0], y=X_train_save[:, 1],
+                    mode='markers',
+                    name='Real Data',marker_color='Green',marker=dict(size=7,color='Green')))
+
+    fig.add_trace(go.Scatter(x=gen[:, 0], y=gen[:, 1],
+                    mode='markers',
+                    name='Fake data',marker_color='Red',marker=dict(size=10,color='Red',symbol = 'cross')))
+    
+    fig.update_layout(
+        autosize=True,
+        width=700,
+        height=700)
+
+    return fig
