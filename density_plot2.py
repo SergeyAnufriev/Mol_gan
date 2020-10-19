22a61ff966c9ed478843d3ffd0cb5478e7c5bbb6
Sergey Anufriev <35104456+SergeyAnufriev@users.noreply.github.com>
2020-10-19 17:01:32 +0300

    Update density_plot2.py

--- density_plot2.py
+++ density_plot2.py
@@ -42,8 +42,9 @@ def get_esd_plot2(T):
     plt.yticks(fontsize=12)
     plt.axis([np.min(eigenvalues) - 1, np.max(eigenvalues) + 1, None, None])
     plt.tight_layout()
-    plt.savefig('example.pdf')
-    plt.show()
+    #plt.savefig('example.pdf')
+    #plt.show()
+    return plt
 
 
 def density_generate(eigenvalues,
